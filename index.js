d223b14f1e6ac86c368a75050b2e565c394b8a4e
Agayby <peter.m.agayby@gmail.com>
2018-11-27 13:56:57 +0200

    Adding body-parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+//parsing request bodies
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log(`Server running on port 3000`);
