25ceb9442c1dac20763fb2bf1840bb3e81b5229f
Agayby <peter.m.agayby@gmail.com>
2018-11-27 13:58:13 +0200

    Adding cors

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 var app = express();
 //parsing request bodies
 app.use(bodyParser.json());
+
+app.use(
+    cors({
+      origin: true,
+      credentials: true,
+      methods: ['GET', 'POST', 'PATCH', 'DELETE']
+    })
+  );
+
 
 app.listen(3000, () => {
     console.log(`Server running on port 3000`);
